b4be42381d5db2aef17ee38551e0774af755cc5d
taocv56 <chutao1993@gmail.com>
2014-12-22 16:14:50 +0700

    Add password resets & email configuration

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20141222064624) do
     t.string   "remember_digest"
     t.boolean  "admin",             default: false
     t.string   "activation_digest"
-    t.boolean  "activated"
+    t.boolean  "activated",         default: false
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
