1d2f77f8708576033af036e6bfaf042f5c32b38b
taocv56 <chutao1993@gmail.com>
2015-01-20 15:49:56 +0700

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ SampleApp::Application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'blog-system.herokuapp.com'
+  host = '<your heroku app>.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   # ActionMailer::Base.smtp_settings = {
     # :address        => 'smtp.sendgrid.net',
